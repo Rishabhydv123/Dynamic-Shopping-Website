f9ce02087b0f714f557648615cb76018c1886a81
Rishabhydv123 <rishabhyadav55888@gmail.com>
2026-03-09 21:59:57 +0530

    update

---
+++ JavaScript/Login.js
@@ -0,0 +1,23 @@
+const storeData = JSON.parse(localStorage.getItem("userData")) || [];
+
+function Login() {
+  const userName = document.getElementById("loginUsername").value.trim();
+  const password = document.getElementById("Loginpassword").value.trim();
+
+  const findUser = storeData.find(el => el.user === userName);
+
+  if (!findUser) {
+    alert("User not found ❌");
+    return;
+  }
+
+  if (findUser.pass !== password) {
+    alert("Password doesn't match ❌");
+    return;
+  }
+
+  sessionStorage.setItem("token", "loggedin");
+  sessionStorage.setItem("loggedUser", userName);
+  // alert("Login Successful ✅");
+  window.location.href = "/local storage/page/home.html";
+}
